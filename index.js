5b93a43d873d428221267113c513ac538577c9bd
ninsy <kwojciechowski094@gmail.com>
2017-02-13 23:57:32 +0100

    Test

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var config = require("./server/config/config");
 var app = require("./server/server");
 
 app.listen(config.port);
-console.log(`Magic happens of http://localhost:${config.port}`);
+console.log(`Magic happens on http://localhost:${config.port}`);

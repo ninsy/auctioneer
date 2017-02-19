016ae6ac12c197493006b2982f43930051fec57e
ninsy <kwojciechowski094@gmail.com>
2017-02-19 22:37:35 +0100

    File upload setup done

--- server/api/file/fileController.js
+++ server/api/file/fileController.js
@@ -1,3 +1,17 @@
-exports.uploadFile = function(req, res, next) {
+var googleClient = require("./googleClient");
 
+exports.uploadFile = function(req, res, next) {
+    googleClient.uploadFile(req.file).then(function(fileUrl) {
+        res.json({fileUrl: fileUrl});
+    })
+};
+
+exports.downloadFile = function (req, res, next) {
+    if(!req.query.fileUrl) {
+        res.status(400).json({message: `File URL has to be provided in query string.`});
+    } else {
+        googleClient.downloadFile(req.query.fileUrl).then(function(file) {
+            file.pipe(res);
+        }).catch(next);
+    }
 };
